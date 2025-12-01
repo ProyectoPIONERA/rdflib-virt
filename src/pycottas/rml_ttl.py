846cc9bc6ca78720c3c35f4aca35e08383abc307
Jorge Riaño <jorgerianodelacruz@gmail.com>
2025-12-01 14:02:43 +0100

    upgraded df2ttl

--- src/pycottas/rml_ttl.py
+++ src/pycottas/rml_ttl.py
@@ -16,7 +16,11 @@ def rml_df_to_ttl(csv_path, ttl_path):
     Convierte un CSV de rml_df a un mapping Turtle RML formal.
     Genera predicateObjectMap exactamente según object_map_value y predicate_map_value.
     """
-    
+    rml = "http://w3id.org/rml/"
+    ub = "http://swat.cse.lehigh.edu/onto/univ-bench.owl#"
+    rr = "http://www.w3.org/ns/r2rml#"
+    rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+
 
     df = pd.read_csv(csv_path)
 
@@ -33,24 +37,39 @@ def rml_df_to_ttl(csv_path, ttl_path):
             # Logical source
             ls_type = str(row.get("logical_source_type", "")).split("/")[-1].lower()
             ls_value = str(row.get("logical_source_value"))
-            f.write(f' <{tm_id}> rml:logicalSource [ rml:{ls_type} """{ls_value}""" ')
+            f.write(f' <{tm_id}> rml:logicalSource [ rml:{ls_type} ')
             if ls_type == "query":
-                f.write(f"\n        rml:referenceFormulation rml:SQL2008 ] ;\n")
+                f.write(f' """{ls_value}"""  \n            rml:referenceFormulation rml:SQL2008 ] ;\n')
             else : 
-                f.write(f"] ;\n")
+                f.write(f' "{ls_value}" ] ;\n')
 
             # Predicate Object Map
-            f.write("    rml:predicateObjectMap [\n")
-
+            graph_map_type = str(row.get("graph_map_type", "")).split("/")[-1].lower()
+            graph_map_val = str(row.get("graph_map_value")).split("/")[-1].lower()
+            f.write("    rml:predicateObjectMap")
+            #if "http" in graph_map_type :
+            if graph_map_val and graph_map_type != None:
+                f.write(f'[ rml:graphMap [ rml:{graph_map_type} rml:{graph_map_val} ;\n                rml:termType rml:IRI ] ;\n')
+            else: 
+                f.write(f'[ rml:graphMap [ rml:constant rml:defaultGraph ;\n               rml:termType rml:IRI ] ;\n')
+                
             # Object Map
             obj_type = str(row.get("object_map_type", "")).split("/")[-1].lower()
             obj_val = row.get("object_map_value")
-            f.write(f"        rml:objectMap [ rml:{obj_type} {obj_val} ]\n")
+            if not "http" in obj_val:
+                f.write(f'        rml:objectMap [ rml:{obj_type} "{obj_val}";\n                rml:termType rml:IRI ] ;\n')
+            else:
+                f.write(f'        rml:objectMap [ rml:{obj_type} ub:{str(obj_val).split("#")[-1].lower()} ;\n                rml:termType rml:IRI ] ;\n')
 
             # Predicate Map
             pred_type = str(row.get("predicate_map_type", "")).split("/")[-1].lower()
             pred_val = row.get("predicate_map_value")
-            f.write(f"        rml:predicateMap [ rml:{pred_type} {pred_val} ]\n")
+            if rdf in pred_val:
+                f.write(f'        rml:predicateMap [ rml:{pred_type} rdf:{str(pred_val).split("#")[-1].lower()} ;\n                rml:termType rml:IRI ] ;\n')
+            elif ub in pred_val:
+                f.write(f'        rml:predicateMap [ rml:{pred_type} ub:{str(pred_val).split("#")[-1].lower()} ;\n                rml:termType rml:IRI ] ;\n')                
+
+            #Close Predicate object map
             f.write("    ] ;\n")
 
             # Subject Map
